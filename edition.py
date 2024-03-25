ff2b24f5b7b3aa5e3857a8eb2537dc6b57d8ef93
le-shammer <le-shammer@users.noreply.github.com>
2024-03-25 11:29:58 +0100

    autopep + col width

--- edition.py
+++ edition.py
@@ -24,18 +24,27 @@ def transform_data(df):
                 if col.startswith('hs') and col != 'hs'+letter:
                     other_letter = col.replace('hs', '')
                     main_text = row['hs{}'.format(letter)]
-                    additional_text = '{}: {}'.format(other_letter.upper(), row['hs{}'.format(other_letter.lower())])
-                    distance_column = [c for c in df.columns if "levd" in c and letter in c and other_letter in c][0]
+                    additional_text = '{}: {}'.format(
+                        other_letter.upper(), row['hs{}'.format(other_letter.lower())])
+                    distance_column = [
+                        c for c in df.columns if "levd" in c and letter in c and other_letter in c][0]
                     distance = row[distance_column]
-                    wmd_column = [c for c in df.columns if "wmd" in c and letter in c and other_letter in c][0]
+                    wmd_column = [
+                        c for c in df.columns if "wmd" in c and letter in c and other_letter in c][0]
                     wmd = row[wmd_column]
-                    data.append([index+1, main_text, additional_text, distance, wmd])
+                    data.append(
+                        [index+1, main_text, additional_text, distance, wmd])
         col_name_reference = 'Referenz-Handschrift Hs. ' + letter.upper()
-        transformed_data[letter] = pd.DataFrame(data, columns=['Vers', col_name_reference, 'Vergleichshandschriften', 'Distance', "wmd"])
-        transformed_data[letter]['Vergleichshandschriften mit Distanzen'] = transformed_data[letter]['Vergleichshandschriften'] + ' (levd:' + transformed_data[letter]['Distance'].apply(lambda x: '{:.2f}'.format(x)) + "; wmd: " + transformed_data[letter]['wmd'].apply(lambda x: '{:.2f}'.format(x)) + ')'
+        transformed_data[letter] = pd.DataFrame(data, columns=[
+                                                'Vers', col_name_reference, 'Vergleichshandschriften', 'Distance', "wmd"])
+        transformed_data[letter]['Vergleichshandschriften mit Distanzen'] = transformed_data[letter]['Vergleichshandschriften'] + \
+            ' (levd: ' + transformed_data[letter]['Distance'].apply(lambda x: '{:.2f}'.format(
+                x)) + '; wmd: ' + transformed_data[letter]['wmd'].apply(lambda x: '{:.2f}'.format(x)) + ')'
     return transformed_data
 
 # Function to filter data
+
+
 def filter_data(min_threshold, max_threshold, min_wmd_threshold, max_wmd_threshold, df, hide_extra_verses, show_distances=False):
     filtered_df = df.copy()
     if show_distances:
@@ -45,9 +54,12 @@ def filter_data(min_threshold, max_threshold, min_wmd_threshold, max_wmd_thresho
     col_name_reference = [c for c in df.columns if 'Referenz' in c][0]
     if hide_extra_verses:
         filtered_df = filtered_df[filtered_df[col_name_reference] != na_text]
-    filtered_df.loc[(filtered_df['Distance'] < min_threshold) | (filtered_df['Distance'] > max_threshold) | (filtered_df['wmd'] < min_wmd_threshold) | (filtered_df['wmd'] > max_wmd_threshold), compare_columns] = ''
-    grouped_df = filtered_df.groupby(['Vers', col_name_reference])[compare_columns].apply(lambda x: '<br>'.join(x)).reset_index()
+    filtered_df.loc[(filtered_df['Distance'] < min_threshold) | (filtered_df['Distance'] > max_threshold) | (
+        filtered_df['wmd'] < min_wmd_threshold) | (filtered_df['wmd'] > max_wmd_threshold), compare_columns] = ''
+    grouped_df = filtered_df.groupby(['Vers', col_name_reference])[
+        compare_columns].apply(lambda x: '<br>'.join(x)).reset_index()
     return grouped_df
+
 
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
@@ -66,7 +78,8 @@ app.layout = html.Div([
     html.Div([
         dbc.Row([
             dbc.Col([
-                dbc.Button("Filtermenü", id="toggle-sidebar", className="mb-3"),
+                dbc.Button("Filtermenü", id="toggle-sidebar",
+                           className="mb-3"),
                 dbc.Collapse(
                     dbc.Card(
                         dbc.CardBody([
@@ -100,15 +113,17 @@ app.layout = html.Div([
                                 step=0.1,
                                 value=[0.0, 1.0],
                                 marks={i/10: str(i/10) for i in range(11)}
-                            ), 
+                            ),
                             dcc.Checklist(
                                 id='hide-extra-verses',
-                                options=[{'label': '\tZusatzverse ausblenden', 'value': 'hide'}],
+                                options=[
+                                    {'label': '\tZusatzverse ausblenden', 'value': 'hide'}],
                                 value=[]
                             ),
                             dcc.Checklist(
                                 id='show-distances',
-                                options=[{'label': '\tDistanzen einblenden', 'value': 'show'}],
+                                options=[
+                                    {'label': '\tDistanzen einblenden', 'value': 'show'}],
                                 value=[]
                             )
                         ])
@@ -124,6 +139,8 @@ app.layout = html.Div([
 ])
 
 # Callback to toggle sidebar
+
+
 @app.callback(
     Output("sidebar-collapse", "is_open"),
     [Input("toggle-sidebar", "n_clicks")],
@@ -135,6 +152,8 @@ def toggle_sidebar(n, is_open):
     return is_open
 
 # Callback to update filtered texts
+
+
 @app.callback(
     Output('filtered-texts', 'children'),
     [Input('main-text-dropdown', 'value'),
@@ -146,21 +165,25 @@ def toggle_sidebar(n, is_open):
 def update_filtered_texts(main_text, levd_threshold_range, wmd_threshold_range, hide_extra_verses, show_distances=True):
     min_threshold, max_threshold = levd_threshold_range
     min_wmd_threshold, max_wmd_threshold = wmd_threshold_range
-    filtered_df = filter_data(min_threshold, max_threshold, min_wmd_threshold, max_wmd_threshold, transformed_data[main_text], 'hide' in hide_extra_verses, 'show' in show_distances)
+    filtered_df = filter_data(min_threshold, max_threshold, min_wmd_threshold, max_wmd_threshold,
+                              transformed_data[main_text], 'hide' in hide_extra_verses, 'show' in show_distances)
     if not filtered_df.empty:
         table_rows = []
         for i in range(len(filtered_df)):
-            table_rows.append(html.Tr([html.Td(str(filtered_df.iloc[i][col]).replace('<br>', '\n')) for col in filtered_df.columns], style={'background-color': 'lightgrey' if i % 2 == 1 else 'white'}))
-        
+            table_rows.append(html.Tr([html.Td(str(filtered_df.iloc[i][col]).replace(
+                '<br>', '\n')) for col in filtered_df.columns], style={'background-color': 'lightgrey' if i % 2 == 1 else 'white'}))
+
         return html.Div([
             html.H3("Leseansicht nach Filterkriterien:"),
             html.Table([
-                html.Tr([html.Th(col) for col in filtered_df.columns]),
+                html.Tr([html.Th(col, style={'width': '5%' if col == 'Vers' else '35%' if 'Referenz-Handschrift' in col else '60%', 'overflow': 'hidden', 'text-overflow': 'ellipsis'}) for col in filtered_df.columns]),
                 *table_rows
-            ], className='table', style={'whiteSpace': 'pre-wrap'})
+            ], className='table', style={'whiteSpace': 'pre-wrap', 'table-layout': 'fixed', 'width': '100%'})
+
         ])
     else:
         return "No filtered texts found."
+
 
 if __name__ == '__main__':
     app.run_server(debug=True, port="10000", host="0.0.0.0")
